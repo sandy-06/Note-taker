d136a4e0bd33467bf0933754a94e08379ec1acd5
Sandra Sanders <sandy-sanders@msn.com>
2021-11-30 21:21:16 -0700

    bradely fixed front end

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,38 +2,49 @@ const router = require('express').Router();
 const fs = require("fs");
 
 
+const generateUniqueId = require('generate-unique-id');
 
 
 
 router.get('/notes', (req, res) => {
-    
-     fs.readFile('db/db.json', 'utf8', function(error, data){
-        console.log(data);  
-        res.json(data); 
-     })
-    
-    // [].concat(JSON.parse(data))
+
+   fs.readFile('db/db.json', 'utf8',  function (error, data) {
+      console.log(data);
+      res.json(data);
+   })
+
+
+});
+router.post('/notes', (req, res) => {
+
+   const note = req.body;
+   fs.readFile('db/db.json','utf8',  function (error, data) {
+      const id = generateUniqueId(); 
+
+
+      const notesArray = JSON.parse(data);
+      
+      notesArray.push({...note, id});
+      
+
+      fs.writeFile('db/db.json', JSON.stringify(notesArray), function(error, data){
+         console.log(data);  
+         res.json(data); 
+      })
+   })
+});
+
+
+router.delete('/:id', (req, res) => {
+   Note.destroy({
+      where: {
+         id: req.params.id
+      }
    });
-//   router.get('/animals/:id', (req, res) => {
-//     const result = findById(req.params.id, animals);
-//     if (result) {
-//       res.json(result);
-//     } else {
-//       res.send(404);
-//     }
-//   });
-//   router.post('/api/animals', (req, res) => {
-//     // set id based on what the next index of the array will be
-//     req.body.id = animals.length.toString();
-  
-//     //if any data in re.body is incorrect, 400 error back
-//     if (!validateAnimal(req.body)) {
-//       res.status(400).send('The animal is not properly formatted.');
-//     } else {
-//     //req.body is where our incoming content will be
-//     // add animal to json file and animals array in this function
-//     const animal = createNewAnimal(req.body, animals);
-//     res.json(req.body);
-//     }
-//   });
-  module.exports = router;
+});
+
+
+
+
+
+module.exports = router;
